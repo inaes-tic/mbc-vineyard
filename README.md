e87647b8ec910d774aa569666cae7e5b37737897
Adrian Pardini <publico@tangopardo.com.ar>
2014-02-11 17:09:28 -0300

    Initial import.

---
+++ README.md
@@ -0,0 +1,5 @@
+Vineyard, the MBC portal.
+=========================
+
+Vineyard is a thin wrapper around Webkit intented to find all the available
+MBC services on a netwokrk and expose them on a single place.
---
+++ vineyard.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import sys, os, shutil
+import uuid
+import re
+
+from gi.repository import GLib, GObject, Gtk, WebKit, JSCore, Soup
+from xdg.BaseDirectory import xdg_cache_home
+
+class Vineyard(GObject.GObject):
+    def __init__(self):
+        GObject.GObject.__init__(self)
+        self.window = window = Gtk.Window()
+        self.inspectorWindow = Gtk.Window()
+        self.session = WebKit.get_default_session()
+
+        self.webView = webView = WebKit.WebView()
+        window.add(webView)
+        window.set_decorated(False)
+        window.set_title('MBC - Vineyard')
+
+        self.init_settings()
+        self.add_inspector()
+        self.add_cookie_jar()
+
+        webView.connect ("navigation-policy-decision-requested", self.decide_policy_cb)
+        webView.connect ("close-web-view", lambda app: Gtk.main_quit())
+        webView.connect ("load-error", self.load_error_cb)
+        webView.connect ("document-load-finished", self.load_finished_cb)
+        webView.connect ("window-object-cleared", self.window_object_cleared_cb)
+        webView.set_property ("view-mode", WebKit.WebViewViewMode.FULLSCREEN)
+
+        window.connect ("destroy", lambda app: Gtk.main_quit())
+        window.show_all ()
+        window.fullscreen ()
+
+        webView.grab_focus()
+        # replace this with magic.
+        webView.load_uri('http://localhost:3000/')
+
+    def init_settings(self):
+        settings = self.webView.get_settings()
+        for prop in 'enable-accelerated-compositing enable-fullscreen enable-webgl enable-developer-extras'.split(' '):
+            settings.set_property(prop, True)
+
+    def decide_policy_cb (self, view, frame, request, action, decision, data=None):
+        # just a placeholder for now, but will be handy to have custom urls, like
+        # mbc://something
+        # Also, perhaps writing a custom libsoup handler would be better on the long term.
+        uri = request.get_uri()
+        scheme, path=uri.split(':', 1)
+        if scheme != 'mbc':
+            # Will use the default behaviour.
+            return False
+
+        # a dummy example.
+        path = re.sub('^//', '', path)
+        if re.match('^quit/?$', 'quit'):
+            Gtk.main_quit()
+
+        # To stop further processing.
+        return True
+
+    def window_object_cleared_cb (self, view, frame, context, window_object, data=None):
+        # we can inject custom stuff either executing some js or touching the context.
+        vinejs = """
+            window.Vineyard = {
+                services: {},
+                onServiceAdded: function(serviceName, serviceInfo){},
+                onServiceRemoved: function(serviceName, serviceInfo){},
+
+
+            };
+        """
+        view.execute_script (vinejs)
+
+    def load_finished_cb (self, view, frame, data=None):
+        pass
+
+    def load_error_cb (self, view, frame, uri, error, data=None):
+        pass
+
+    def add_inspector(self):
+        def create_inspector_window(inspector, srcwebview, data=None):
+            webview = WebKit.WebView()
+            self.inspectorWindow.add(webview)
+            return webview
+
+        def show_inspector_window(inspector, data=None):
+            self.inspectorWindow.show()
+
+        inspector = self.webView.get_inspector()
+        inspector.connect('inspect-web-view', create_inspector_window)
+        inspector.connect('show-window', show_inspector_window)
+
+    def add_cookie_jar(self):
+        # here we store everything, however it would be best not to do it
+        # if we care about not leaving auth stuff around.
+        cookiejar = Soup.CookieJarText.new( os.path.join(xdg_cache_home, "vineyard_cookies.txt"), False)
+        cookiejar.set_accept_policy(Soup.CookieJarAcceptPolicy.ALWAYS)
+        self.session.add_feature(cookiejar)
+
+
+if __name__ == '__main__':
+    app = Vineyard()
+
+    Gtk.main()
